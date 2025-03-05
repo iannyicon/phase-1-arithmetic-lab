3e9d35bc7b03bd06abff2a7dc37a6b38b776d346
Iann <iconian4@gmail.com>
2025-03-05 13:08:26 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+// Define the variables
+const num1 = 31;
+const num2 = 2;
+
+// Define the multiply function or equation
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 100) + 1; 
+const num3 = 29;
+const num4 = 5;
+
+const mod = num3 % num4
+
+const max = Math.max(1,20)
